016dc1f42b42aee37d2f3c28354abd72ebf1e871
papachanguito <carlos.ahuactzin@alumno.buap.mx>
2018-06-18 13:00:04 -0500

    destruir

---
+++ Assets/Scripts/GOAP/DestruirAgotado.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirAgotado : MonoBehaviour {
+    public bool checarMadera;
+    public bool checarMineral;
+    Inventario recursos;
+
+	// Use this for initialization
+	void Start () {
+        recursos = GetComponent<Inventario>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (checarMadera && recursos.madera <= 0||checarMineral&&recursos.mineral<=0)
+        {
+            Destroy(gameObject);
+        }
+		
+	}
+}
