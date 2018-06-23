23ee275ecb512399871f16258596fb511afd5bb9
danahpa <danahpa@gmail.com>
2018-06-23 17:20:39 -0500

    base dragon

---
+++ Assets/Scripts/FSM/Dragon/Dragon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonStates;
+
+public class Dragon : MonoBehaviour 
+{
+    public FSM fsm;
+
+    // Variables de dragon
+    public int Hambre;
+ 
+
+    void InitMinerData()
+    {
+        Hambre = 0;
+      
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        InitMinerData();
+        
+        // Hay que hacer la fsm del agented
+        fsm = new FSM(gameObject, this);
+
+        // Crear los estados en que puede estar Bob
+        Mining mining = new Mining(this);
+    
+
+        // Agregar eventos a los estados
+        //sleep.AddEvent(EventList.events.dinnerReady);
+
+        // Hay que agregarlos a la FSM
+        fsm.AddState(StateID.Mining, mining);
+     
+
+        // Indicar cual es el estado inicial
+        fsm.ChangeState(StateID.Mining);
+
+        // Activo la fsm
+        fsm.Activate();
+	}
+	
+	void Update () 
+    {
+		if(fsm != null && fsm.IsActive())
+        {
+            fsm.UpdateFSM();
+        }
+	}
+}
---
+++ Assets/Scripts/FSM/Dragon/DragonStates.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DragonStates
+{
+    public enum StateID // Aqui agreguen las claves de cada estado que quieranC:\Users\carlos.ahuactzin\Documents\finalIA\Assets\Scripts\Elfo\Elfo.cs
+    {
+        Mining,
+        DepositInBank,
+        Drinking,
+        Sleeping,
+        Eating
+    }
+
+    //=============================================================
+    //===================================================Mining
+    public class Mining : State
+    {
+        private Dragon dragon;
+
+        // Semaforo o candado para tiempos
+        private bool working;
+        // Una referencia a la corutina
+        private Coroutine workingCoroutine;
+
+        public Mining(Dragon _dragon)
+        {
+            dragon = _dragon;
+        }
+
+        public override void OnEnter(GameObject objeto)
+        {
+           
+        }
+        public override void Act(GameObject objeto)
+        {
+           
+        }
+        public override void Reason(GameObject objeto)
+        {
+           
+        }
+        public override void OnExit(GameObject objeto)
+        {
+           
+        }
+
+    }
+}
+
+    
