1a78a767a026944eeb2226cbba71fb8213373144
danahpa <danahpa@gmail.com>
2018-06-25 10:43:45 -0500

    esto

---
+++ Assets/Scripts/FSM/Orco/Orco.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OrcoStates;
+
+public class Orco : MonoBehaviour 
+{
+    public FSM fsm;
+    public SteeringCombined steering;
+    public GameManagerScript manager;
+
+    // Variables de dragon
+    public int Hambre;
+    public int Cansancio;
+ 
+
+    void InitMinerData()
+    {
+        Hambre = 0;
+      
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        InitMinerData();
+        
+        // Hay que hacer la fsm del agented
+        fsm = new FSM(gameObject, this);
+        // Asignar el steering
+        steering = GetComponent<SteeringCombined>();
+     
+
+        // Crear los estados en que puede estar Bob
+        Deambular deambulando = new Deambular(this);
+     
+    
+
+        // Agregar eventos a los estados
+        //sleep.AddEvent(EventList.events.dinnerReady);
+
+        // Hay que agregarlos a la FSM
+        fsm.AddState(StateID.Deambular, deambulando);
+      
+
+
+        // Indicar cual es el estado inicial
+        fsm.ChangeState(StateID.Deambular);
+
+        // Activo la fsm
+        fsm.Activate();
+	}
+	
+	void Update () 
+    {
+		if(fsm != null && fsm.IsActive())
+        {
+            fsm.UpdateFSM();
+        }
+	}
+}
---
+++ Assets/Scripts/FSM/Orco/OrcoStates.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OrcoStates
+{
+    public enum StateID // Aqui agreguen las claves de cada estado que quieranC:\Users\carlos.ahuactzin\Documents\finalIA\Assets\Scripts\Elfo\Elfo.cs
+    {
+        Deambular,
+        Comer,
+        Dormir,
+        Destruir
+    }
+
+    //=============================================================
+    //===================================================deambular
+    public class Deambular : State
+    {
+        private Orco orco;
+
+        // Semaforo o candado para tiempos
+        private bool Hambre;
+        // Una referencia a la corutina
+        private Coroutine workingCoroutine;
+
+        public Deambular(Orco _orco)
+        {
+            orco = _orco;
+        }
+
+        public override void OnEnter(GameObject objeto)
+        {
+            orco.steering.wander = true;
+            Hambre = false;
+        }
+        public override void Act(GameObject objeto)
+        {
+           
+        }
+        public override void Reason(GameObject objeto)
+        {
+            if (!Hambre)
+            {
+                fsm.myMono.StartCoroutine(HambrientoFunction());
+            }
+
+            if (orco.Hambre >= 10)
+            {
+                ChangeState(StateID.Comer);
+            }
+           
+        }
+        public override void OnExit(GameObject objeto)
+        {
+            orco.steering.wander = false;
+        }
+
+        IEnumerator HambrientoFunction()
+        {
+            Hambre = true;
+            yield return new WaitForSeconds(5f);
+            orco.Hambre += 1;
+            Hambre = false;
+
+
+        }
+
+    }
+
+
+}
+
